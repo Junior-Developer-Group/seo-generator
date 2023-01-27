a7b7910ebb8e72a86cc33c8836503d9b1bc3aba1
A-amon <64846892+A-amon@users.noreply.github.com>
2023-01-27 21:56:30 +0800

    Add aria live to input keywords message

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -10,13 +10,16 @@ inputKeywords.addEventListener("input", (event) => {
 	// Invalid value: (Any value containing special character)
 	if((/[^\w\s,]/g).test(trimmedValue)){
 		inputKeywordsMessage.textContent = "Please enter only alphanumeric characters"
+		inputKeywords.setAttribute("aria-invalid", true)
 	}
 	// Invalid value: keyword1, , keyword2
 	else if((/,\s*,/g).test(trimmedValue)){
 		inputKeywordsMessage.textContent = "Please ensure there is no trailing comma"
+		inputKeywords.setAttribute("aria-invalid", true)
 	}
 	else{
 		inputKeywordsMessage.textContent = ""
+		inputKeywords.removeAttribute("aria-invalid")
 		updateCodeElement("meta-keywords", trimmedValue.length > 0 && `<meta name="keywords" content="${trimmedValue}">`)
 	}
 })
